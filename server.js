8009aa45a53d9fabbdc57f18659b7dafc97d7a80
Oleh <shtepa.oleh.m@gmail.com>
2019-06-28 19:12:20 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cities = require('./data/city.list.json');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const getCityByName = nameOfCity => cities.filter(city => new RegExp(nameOfCity, 'i').test(city.name));
+const isValidRequest = request => request && /^[a-z]+$/i.test(request);
+
+app.get('/api/getCity/', (req, res) => {
+    const requestCity = req.query.name;
+    if (isValidRequest(requestCity)) {
+        res.json({
+            status: 'ok',
+            data: getCityByName(requestCity),
+        });
+    } else {
+        res.json({
+            status: 'Something goes wrong',
+            data: [],
+        });
+    }
+});
+
+app.use('*', (req, res) => res.send(''));
+
+app.listen(port, function() {
+    console.log(`Server is running on ${port}`);
+});
