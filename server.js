9480f59cbec37032f6793e25c4bbde4923298100
Oleh <shtepa.oleh.m@gmail.com>
2019-07-10 17:54:44 +0300

    Allow CORS

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const port = process.env.PORT || 3000;
 const getCityByName = nameOfCity => cities.filter(city => new RegExp(nameOfCity, 'i').test(city.name));
 const isValidRequest = request => request && /^[a-z]+$/i.test(request);
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/api/getCity/', (req, res) => {
     const requestCity = req.query.name;
     if (isValidRequest(requestCity)) {
