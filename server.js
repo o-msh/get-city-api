9effe3094e5cc0888b1f5bb140f4d53d9563a9ae
Oleh <shtepa.oleh.m@gmail.com>
2019-10-05 16:49:09 +0300

    Refactoring server.js

--- server.js
+++ server.js
@@ -3,31 +3,21 @@ const cities = require('./data/city.list.json');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const getCityByName = nameOfCity => cities.filter(city => new RegExp(nameOfCity, 'i').test(city.name));
+const getCityByName = nameOfCity => cities.filter(({ name }) => new RegExp(nameOfCity, 'i').test(name));
 const isValidRequest = request => request && /^[a-z]+$/i.test(request);
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
 app.get('/api/getCity/', (req, res) => {
-    const requestCity = req.query.name;
-    if (isValidRequest(requestCity)) {
-        res.json({
-            status: 'ok',
-            data: getCityByName(requestCity),
-        });
-    } else {
-        res.json({
-            status: 'Something goes wrong',
-            data: [],
-        });
-    }
+    const { name } = req.query;
+    isValidRequest(name) ? res.json({ status: 'ok', data: getCityByName(name) }) : res.json({ status: 'Something goes wrong', data: [] });
 });
 
-app.use('*', (req, res) => res.send(''));
+app.use('*', (req, res) => res.sendStatus(404));
 
 app.listen(port, function() {
     console.log(`Server is running on ${port}`);
